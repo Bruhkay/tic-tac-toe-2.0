f6c06a80bef1a6145cca52271ee4756ea14bc663
Burkay <burkay.tuncturk@gmail.com>
2024-07-21 12:16:30 +0300

    gui added
    some minor improvements needed such as hiding the move which will be vanished in the next move

---
+++ Frame.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Frame extends JFrame {
+    private JButton[][] buttons; // Oyun tahtası için düğmeler
+    private char currentPlayer; // Sıradaki oyuncu (X veya O)
+    Game game;
+
+    int[][] pattern ={{7,8,9}, {4,5,6}, {1,2,3}};
+
+    public Frame() {
+        game = new Game();
+        setTitle("Tic Tac Toe");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        buttons = new JButton[3][3];
+        currentPlayer = 'X';
+
+        initializeButtons();
+        setVisible(true);
+    }
+
+    private void initializeButtons() {
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col] = new JButton();
+                buttons[row][col].setFont(new Font("Arial", Font.BOLD, 60));
+                buttons[row][col].addActionListener(new ButtonClickListener(row, col));
+                add(buttons[row][col]);
+            }
+        }
+    }
+    private class ButtonClickListener implements ActionListener {
+        private int row;
+        private int col;
+
+        public ButtonClickListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+                if(currentPlayer=='X'){
+                    game.pl1.addMove(pattern[row][col]);
+                }
+                else{
+                    game.pl2.addMove(pattern[row][col]);
+                }
+
+                for(int j = 0; j<3; j++){
+                    for(int i= pattern[j][0]; i<=pattern[j][2]; i++){
+                        if(game.pl1.doesContain(i)){
+                            buttons[j][i-pattern[j][0]].setText(String.valueOf('X'));
+                            buttons[j][i-pattern[j][0]].setEnabled(false);
+                            if(i==game.pl1.hand[2]){
+                                buttons[j][i-pattern[j][0]].setFont(new Font("Arial", Font.PLAIN, 36));
+                            }
+                        }
+                        else if(game.pl2.doesContain(i)){
+                            buttons[j][i-pattern[j][0]].setText(String.valueOf('O'));
+                            buttons[j][i-pattern[j][0]].setEnabled(false);
+                            if(i==game.pl2.hand[2]){
+                                buttons[j][i-pattern[j][0]].setFont(new Font("Arial", Font.PLAIN, 36));
+                            }
+                        }
+                        else{
+                            buttons[j][i-pattern[j][0]].setText("");
+                            buttons[j][i-pattern[j][0]].setEnabled(true);
+                        }
+                    }
+                }
+                game.display();
+                checkForWinner();
+                if (currentPlayer == 'X') {
+                    currentPlayer = 'O';
+                } else {
+                    currentPlayer = 'X';
+                }
+
+        }
+        public void checkForWinner(){
+            if(game.gameFinished()){
+                System.out.println("oyun bitii");
+                setVisible(false);
+            }
+        }
+    }
+}
+
--- Main.java
+++ Main.java
@@ -1,46 +1,9 @@
+import javax.swing.*;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]){
-        Game game = new Game();
-        Random rn = new Random();
-        Scanner sn = new Scanner(System.in);
-        int randomNumber = rn.nextInt(2);
-        int number;
-
-        while(!game.gameFinished()){
-            game.display();
-            if(randomNumber == 0){
-                System.out.print("Turn of player 1: ");
-                do {
-                    number = sn.nextInt();
-                    if(game.pl1.doesContain(number) || game.pl2.doesContain(number)){
-                        System.out.println("Already selected!!!!");
-                    }
-                }while(game.pl1.doesContain(number) || game.pl2.doesContain(number));
-                game.pl1.addMove(number);
-                randomNumber = 1;
-            }
-            else {
-                System.out.print("Turn of player 2: ");
-                do {
-                    number = sn.nextInt();
-                    if(game.pl1.doesContain(number) || game.pl2.doesContain(number)){
-                        System.out.println("Already selected!!!!");
-                    }
-                }while(game.pl1.doesContain(number) || game.pl2.doesContain(number));
-                game.pl2.addMove(number);
-                randomNumber = 0;
-            }
-        }
-
-        game.display();
-        if(game.pl1.didFinish()){
-            System.out.println("Winner is player 1!!!");
-        }
-        else{
-            System.out.println("Winner is player 2!!!");
-        }
+        JFrame frame = new Frame();
     }
 }
